308872d1c2a134a6d585032850d7b328241520a6
Tarunkumar853 <tarunkumarpal253@gmail.com>
2025-10-27 10:21:05 +0530

    Add files via upload

---
+++ tarun_kumar_st1.html.h
@@ -0,0 +1,29 @@
+<html>
+<title>Ecommerce Website</tltle>
+<body>
+	<h1 style="text-align: center;">Buy-Buy Mart</h1>
+		<nav id="#1">Services</nav>
+		<nav id="#2">Offices</nav>
+		<nav id="#3">Products</nav>
+		<nav id="#4">Co-Partners</nav>
+		<nav id="#5">Contact Us</nav>
+	<hr>
+	<h3 id="#1">Services</h3>
+	<p> we deal all the financial problem by standing with our customer</p>
+		<img src="image.jpg" type="image/jpg" height ="300" width="300">
+		<img src="employee.jpg" type="image/jpg" height="300" width="300">
+	<hr>
+	<h3 id="#2">Offices</h3>
+	<p> In our India, We have 2 Lakhs+ offices in different states for helping our customers</p>
+		<img src="office.jpg" type="image/jpg" height ="300" width="300">
+		<video playback src="differenent office.mp4" type="video/mp4">
+	<hr>
+	<h3 id="#3">Products</h3>
+	<p> We Provide our products and services all over the india, We have many different products. Some Products and their Purchase Links Given Below</p>
+	<a href="www.xyz.com"><img src="prt1.jpg" height="300" width="300" type="image/jpg></a>
+	<a href="www.abc.com"><img src="prt2.jpg" height ="300" width="300" type="image/jpg"></a>
+	<a href="www.abd.com"><img src="prt3.jpg" height ="300" width="300" type="image/jpg"></a>
+	
+<body>
+	
+</html>
